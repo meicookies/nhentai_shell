87dd4d208ccf37f83be2561affb481a3d24be526
meicookies <79348524+meicookies@users.noreply.github.com>
2021-09-15 06:35:05 +0700

    Shhhhhh

--- nhentai.sh
+++ nhentai.sh
@@ -20,19 +20,19 @@ site="https://nhentai.net"
 letakpict="https://i.nhentai.net/galleries"
 
 thumbnail=$(curl -X GET --silent $site/g/$1/ \
-	| htmlq --pretty '#thumbnail-container' --attribute data-src img)
+  | htmlq --pretty '#thumbnail-container' --attribute data-src img)
 
 if [[ -n "$thumbnail" ]]; then
   mkdir $1
 
-	url_=$(echo "$thumbnail" | head -1)
-	getnumidk=$(basename $(dirname $url_))
+  url_=$(echo "$thumbnail" | head -1)
+  getnumidk=$(basename $(dirname $url_))
 
-	for dwn in $(seq 1 $(echo "$thumbnail" | wc -l)); do
-		wget -q --show-progress -P $1 $letakpict/$getnumidk/$dwn.jpg &
-	done; wait
+  for dwn in $(seq 1 $(echo "$thumbnail" | wc -l)); do
+  wget -q --show-progress -P $1 $letakpict/$getnumidk/$dwn.jpg &
+  done; wait
 
-	echo -e "All done\nSaved into $1 directory"
+  echo -e "All done\nSaved into $1 directory"
 else
   echo "oops sorry dude I didn't find any doujin with this code [$1]"
 fi
